6847a896e1c01ceb9cf5cff3ffdba29abd6f9b0a
manish <manish.lapasi@gmail.com>
2022-05-31 01:31:58 +0530

    UCB algo original

--- models/RL/UCBalgo.py
+++ models/RL/UCBalgo.py
@@ -11,10 +11,13 @@ def del_i(n,N):
 N_users = 10000
 N_ads = 10
 rewards_so_far = dataset[0,:]
-N_so_far = list(N_ads*[1])
-UCB = list([0+del_i(i,1)] for i in range(1,N_ads+1))
+N_so_far = list(N_ads*[0])
+UCB = list([0+del_i(i+1,1)] for i in range(0,N_ads))
+UCB = [i for [i] in UCB]
 current_round = N_ads+1
 max_rounds_allowed = 10000
+print(N_so_far)
+total_reward = 0
 
 for current_round in range(N_ads,max_rounds_allowed+1):
     current_ad = UCB.index(max(UCB))
@@ -22,7 +25,13 @@ for current_round in range(N_ads,max_rounds_allowed+1):
     rewards_so_far[current_ad] = rewards_so_far[current_ad] + dataset[N_so_far[current_ad]][current_ad]
     r_avg = rewards_so_far[current_ad] / N_so_far[current_ad]
     UCB[current_ad] = r_avg + del_i(current_round, N_so_far[current_ad])
+    total_reward = total_reward + dataset[N_so_far[current_ad]][current_ad]
     
 print(UCB)
 print(UCB.index(max(UCB)))
 print(N_so_far)
+print(total_reward)
+
+import matplotlib.pyplot as plt
+plt.bar([i for i in range(N_ads)],N_so_far)
+plt.show()
---
+++ models/RL/UCBalgo2.py
@@ -0,0 +1,43 @@
+from cmath import log
+import math
+import numpy as np, pandas as pd
+
+dataset = pd.read_csv("../../resources/Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/Python/Ads_CTR_Optimisation.csv")
+dataset = dataset.values
+
+def del_i(n,N):
+    return math.sqrt((3*math.log(n))/(2*N))
+
+N_ads = 10
+max_rounds_allowed = 10000
+ads_selected = []
+number_of_selections = [0] * N_ads
+sums_of_rewards = [0] * N_ads
+total_reward = 0 ## this is missing
+    
+for n in range(0,max_rounds_allowed):
+    ad = 0
+    max_upper_bound = 0
+    for i in range(0, N_ads):
+        if(number_of_selections[i] > 0):
+            average_reward = sums_of_rewards[i] / number_of_selections[i]
+            delta_i = del_i(n+1,number_of_selections[i])
+            upper_bound = average_reward + delta_i
+        else:
+            upper_bound = 1e400
+        if(upper_bound > max_upper_bound):
+            max_upper_bound = upper_bound
+            ad = i
+    ads_selected.append(ad)
+    number_of_selections[ad] = number_of_selections[ad] + 1
+    reward = dataset[n,ad]
+    sums_of_rewards[ad] = sums_of_rewards[ad] + reward
+    total_reward = total_reward + reward
+
+import matplotlib.pyplot as plt
+plt.hist(ads_selected)
+plt.show()
+
+shown_so_far = [ads_selected.count(i) for i in range(0,10)]
+print(shown_so_far)
+print(total_reward)
