f3d36c3f94225045672725a4cff0408be77bdd84
manish <manish.lapasi@gmail.com>
2022-06-11 17:08:32 +0530

    pca, lda, kernel pca, kfold, gridsearch

---
+++ models/dimensionality_reduction/pca.py
@@ -0,0 +1,34 @@
+import random
+import numpy as np, pandas as pd
+
+dataset = pd.read_csv("../../resources/Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/Python/Wine.csv")
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+print(np.unique(y))
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+ss_x = StandardScaler()
+x_train = ss_x.fit_transform(x_train)
+x_test = ss_x.transform(x_test)
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+x_train = pca.fit_transform(x_train)
+x_test = pca.transform(x_test)
+
+from sklearn.linear_model import LogisticRegression
+logReg = LogisticRegression(random_state=0)
+logReg.fit(x_train, y_train)
+
+print(logReg.coef_)
+
+y_pred = logReg.predict(x_test)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+acc = accuracy_score(y_test, y_pred)
+print(cm,acc)
---
+++ models/model_selection/gridSearch.py
@@ -0,0 +1,38 @@
+from matplotlib.pyplot import grid
+import numpy as np, pandas as pd
+
+dataset = pd.read_csv("../../resources/Part 10 - Model Selection _ Boosting/Section 48 - Model Selection/Python/Social_Network_Ads.csv")
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+from sklearn.preprocessing import StandardScaler
+ss_x = StandardScaler()
+x_train = ss_x.fit_transform(x_train)
+x_test = ss_x.transform(x_test)
+
+from sklearn.svm import SVC
+svc = SVC(kernel='rbf',random_state=0)
+svc.fit(x_train, y_train)
+y_pred = svc.predict(x_test)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+acc = accuracy_score(y_test, y_pred)
+print(cm, acc)
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=svc, X = x_train, y = y_train, cv = 10)
+print("Mean accuracy : {:.2f} %".format(accuracies.mean()*100))
+print("Mean STD : {:.2f} %".format(accuracies.std()*100))
+
+from sklearn.model_selection import GridSearchCV
+parameters = [
+    {'C':[0.25,0.5,0.75,1], 'kernel':['linear']},
+    {'C':[0.25,0.5,0.75,1], 'kernel':['rbf'], 'gamma': [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]}]
+grid_search = GridSearchCV(estimator=svc, param_grid=parameters, scoring='accuracy', cv=10, n_jobs=-1)
+grid_search.fit(X=x_train, y=y_train)
+print("Best accuracy : {:.2f} %".format(grid_search.best_score_*100))
+print("Best parameters : ",grid_search.best_params_)
